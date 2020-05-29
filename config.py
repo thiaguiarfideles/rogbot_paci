f52e2dcf5965bb23154342726e9f7fd818218a94
thiaguiarfideles <thiaguiarfideles@live.com>
2020-05-29 16:02:50 -0300

    Initial comment

---
+++ config.py
@@ -0,0 +1,8 @@
+# The absolute path of the directory containing images for users to download
+app.config["ROGBOT_IMAGES"] = "Users/THIAG/PycharmProjects/ROGBOT_/static/rogbot/img"
+
+# The absolute path of the directory containing CSV files for users to download
+app.config["ROGBOT_CSV"] = "Users/THIAG/PycharmProjects/ROGBOT_/static/rogbot/csv"
+
+# The absolute path of the directory containing PDF files for users to download
+app.config["ROGBOT_PDF"] = "Users/THIAG/PycharmProjects/ROGBOT_/static/rogbot/pdf"
